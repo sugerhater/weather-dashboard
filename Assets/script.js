3b4971e5a1a255433d15f611aa9642885553ef2f
sugerhater <kehong0926@gmail.com>
2020-11-21 23:44:41 -0500

    finished top right section

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,16 @@
-var cities = [];
+var cities = ["Columbus"];
 
 var APIKey = "&appid=89e03da76afa1f1ca74f57becbb1ae3d";
 var url = "https://api.openweathermap.org/data/2.5/weather?q="
 
-
 function renderButtons() {
     $("#city-list").empty();
-
     // for (var i = cities.length;i <0; i--){
     for (var i = 0; i < cities.length; i++) {
         var eachCity = $("<button>");
         eachCity.attr("data-name", cities[i]);
         eachCity.text(cities[i]);
+        eachCity.addClass("btn btn-secondary")
         $("#city-list").append(eachCity);
         $("#city-list").append($("<br>"));
 
@@ -20,47 +19,60 @@ function renderButtons() {
     console.log(cities);
 }
 
-function showWeather(){
-    // var CurrentCity = cities[0];
-    // var BJ = "bew,usa-OH";
-    var queryURL = url+cities[0] +APIKey;
+function showWeather() {
+    var queryURL = url + cities[0] + APIKey;
     console.log(cities[0]);
     var CurrentCityWeather = $("#current-weather-view");
-    // CurrentCityWeather.empty();
-
-
+    
     $.ajax({
-        url:queryURL,
-        method:"GET"
-    }).then(function(response){
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
         console.log(response)
-      $(".wind").html("Wind Speed: "+(response.wind.speed*2.236).toFixed(1)+" MPH")
+        var highTemp = (response.main.temp_max * 9 / 5 - 459.67).toFixed(1);
+        var lowTemp = (response.main.temp_min * 9/5 - 459.67).toFixed(1);
+        $(".city").html('<h1>' + response.name + '</h1>');
+        $(".temp").html('Temperature: ' + highTemp + 'F / ' + lowTemp +'F')
+        $(".wind").html("Wind Speed: " + (response.wind.speed * 2.236).toFixed(1) + " MPH")
+        $(".humidity").html("Humidity: " + response.main.humidity + "%");
+        
         console.log("AJAX is called")
         
-
+        
     });
-
+    
 }
-
-
 
 
 $("#search-city").on("click", function (event) {
     event.preventDefault();
     var cityName = $("#city-input").val();
-    console.log(cityName);
-    cities.unshift(cityName);
+    var queryURL = url + cityName + APIKey;
+    
+    $.ajax({
+        url : queryURL,
+        method : "GET"
+    }).then(function(response){
+        cityName = response.name;
+        if (cities.indexOf(cityName)== -1){
+            cities.unshift(cityName);
+        }
+        console.log(cities);
+        renderButtons();
+        showWeather();
 
-    renderButtons();
+    })
 
-    showWeather();
+
+
+
 
 
 
 })
 
 renderButtons();
-
+showWeather();
 // showWeather();
 
     // function search(){
